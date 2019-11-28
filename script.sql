150627b54839946db662e68c87cec4c1c81d015f
Nikitas Nikolay <kolya.nikitas@yandex.ru>
2019-11-29 00:03:01 +0500

    Added sql script

---
+++ script.sql
@@ -0,0 +1,35 @@
+create table users
+(
+    uuid     uuid not null
+        constraint users_pk
+            primary key,
+    login    text not null,
+    password text not null,
+    salt     text not null
+);
+
+alter table users
+    owner to kolya59;
+
+create unique index users_login_uindex
+    on users (login);
+
+create unique index users_uuid_uindex
+    on users (uuid);
+
+create table tasks
+(
+    uuid        uuid    not null
+        constraint tasks_pk
+            primary key,
+    value       text,
+    author_uuid uuid    not null,
+    is_resolved boolean not null
+);
+
+alter table tasks
+    owner to kolya59;
+
+create unique index tasks_uuid_uindex
+    on tasks (uuid);
+
